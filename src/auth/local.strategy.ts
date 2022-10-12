6071d24b49918b909220dc100748e9db2625ef85
chwech <804699297@qq.com>
2022-10-12 11:42:39 +0800

    fix: 修改登录不成功的错误返回

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -12,12 +12,14 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   // 实现valitate验证
   async validate(username: string, password: string): Promise<any> {
-    // 验证不通过, 抛出错误
-    // 验证成功返回用户信息
     const user = await this.authService.validateUser(username, password);
+
+    // 验证不通过, 抛出错误
     if (!user) {
-      throw new UnauthorizedException();
+      throw new BadRequestException('账号或密码错误');
     }
+
+    // 验证成功返回用户信息
     return user;
   }
 }
